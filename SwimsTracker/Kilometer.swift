1b313113c61518f5ebfbc5ed327bdf6c812a3101
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-23 19:57:00 +1000

    Add Swim model

---
+++ SwimsTracker/Kilometer.swift
@@ -0,0 +1,9 @@
+//
+//  Kilometer.swift
+//  SwimsTracker
+//
+//  Created by Giovanni on 23/08/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+typealias Kilometer = Double
---
+++ SwimsTracker/Swim.swift
@@ -0,0 +1,16 @@
+//
+//  Swim.swift
+//  SwimsTracker
+//
+//  Created by Giovanni on 23/08/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import Foundation
+
+struct Swim {
+
+  let date: NSDate
+  let length: Kilometer
+  let duration: NSTimeInterval
+}
