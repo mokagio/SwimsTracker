40f2c813555481ef5192c737e97059ad19938594
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-23 12:58:16 +1000

    Add dummy table view to swim list VC

--- SwimsTracker/SwimListViewController.swift
+++ SwimsTracker/SwimListViewController.swift
@@ -10,7 +10,49 @@ import UIKit
 
 class SwimListViewController: UIViewController {
 
+  @IBOutlet var tableView: UITableView!
+
+  let dummyData: [String] = { (0...42).map { "Row \($0)" } }()
+
+  let cellIdentifier = "cell"
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    automaticallyAdjustsScrollViewInsets = false
+
+    title = "🏊"
+    configureTableView()
+  }
+
+  private func configureTableView() {
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+  }
+}
+
+extension SwimListViewController: UITableViewDataSource {
+
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dummyData.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+    let dummyValue = dummyData[indexPath.row]
+    cell.textLabel?.text = dummyValue
+    return cell
+  }
+}
+
+extension SwimListViewController: UITableViewDelegate {
+
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
 }
