c7a35d566458640da19215e39a5c9fffc059a7e8
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-23 12:42:39 +1000

    🏊 SwimsTracker first commit: template Xcode project

---
+++ SwimsTracker/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SwimsTracker
+//
+//  Created by Giovanni on 23/08/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+}
---
+++ SwimsTracker/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwimsTracker
+//
+//  Created by Giovanni on 23/08/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
