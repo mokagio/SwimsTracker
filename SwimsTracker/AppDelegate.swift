6f7c4e76f96a9910cbbe202f09847a6057296be1
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-25 05:44:38 +1000

    Initialise SwimsService in the AppDelegate

--- SwimsTracker/AppDelegate.swift
+++ SwimsTracker/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  let swimsService = SwimsService()
+
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     return true
   }
